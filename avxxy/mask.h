239ca0cf9d8ef933c239c7dba8d9d85a55451424
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-10 04:27:00 +0500

    added utils,

--- avxxy/mask.h
+++ avxxy/mask.h
@@ -3,12 +3,12 @@
 #include "backends.h"
 namespace AVXXY_NAMESPACE
 {
-	template<typename _S, size_t _N, typename Backend = backends::current>
+	template<size_t N>
 	struct SIMD_Mask
 	{
 	public:
-		static inline constexpr size_t BitCount = _N;
-		using ScalarType = _S;
+		static inline constexpr size_t BitCount = N;
 	private:
+		//bit mask
 	};
 }
