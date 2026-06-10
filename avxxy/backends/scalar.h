c9410055f5ea9dae9da49651a964756690903aa3
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-10 23:18:47 +0500

    Scalar backend: unapck_base is now private

--- avxxy/backends/scalar.h
+++ avxxy/backends/scalar.h
@@ -57,7 +57,7 @@ namespace AVXXY_NAMESPACE
 
 			//TODO: limit bitwise operations to int types?
 			template<typename S, size_t N>
-			static SIMD_Vector<S, N> logic_or(const SIMD_Vector<S, N>&a, const SIMD_Vector<S, N>&b)
+			static SIMD_Vector<S, N> logic_or(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 			{
 				SIMD_Vector<S, N> ret;
 				using T = same_size_uint_t<S>::type;
@@ -213,24 +213,6 @@ namespace AVXXY_NAMESPACE
 				return ret;
 			}
 
-			template<typename S, size_t N, bool Lo>
-			static SIMD_Vector<S, N> unpack_base(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
-			{
-				SIMD_Vector<S, N> ret;
-				constexpr size_t pairs_per_xmm = 8 / sizeof(S); //8, since unpack only processes lower/upper half of each input
-				constexpr size_t elements_per_xmm = 16 / sizeof(S); //how much elements of type S fit into one 128 bit lane
-				constexpr size_t xmm_count = sizeof(ret) / 16;
-				for (size_t xmm_i = 0; xmm_i < xmm_count; ++xmm_i) //for each 128-bit lane
-				{
-					for (size_t i = 0; i < elements_per_xmm; i += 2)
-					{
-						size_t srcI = xmm_i * elements_per_xmm + i / 2 + (Lo ? 0 : elements_per_xmm / 2);
-						ret[xmm_i * elements_per_xmm + i] = a[srcI];
-						ret[xmm_i * elements_per_xmm + i + 1] = b[srcI];
-					}
-				}
-				return ret;
-			}
 			template<typename S, size_t N>
 			static SIMD_Vector<S, N> unpacklo(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 			{
@@ -476,6 +458,26 @@ namespace AVXXY_NAMESPACE
 			{
 				SIMD_Vector<float, N> ret;
 				for (size_t i = 0; i < N; ++i) ret[i] = fp16_to_fp32(a[i]);
+				return ret;
+			}
+
+		private:
+			template<typename S, size_t N, bool Lo>
+			static SIMD_Vector<S, N> unpack_base(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
+			{
+				SIMD_Vector<S, N> ret;
+				constexpr size_t pairs_per_xmm = 8 / sizeof(S); //8, since unpack only processes lower/upper half of each input
+				constexpr size_t elements_per_xmm = 16 / sizeof(S); //how much elements of type S fit into one 128 bit lane
+				constexpr size_t xmm_count = sizeof(ret) / 16;
+				for (size_t xmm_i = 0; xmm_i < xmm_count; ++xmm_i) //for each 128-bit lane
+				{
+					for (size_t i = 0; i < elements_per_xmm; i += 2)
+					{
+						size_t srcI = xmm_i * elements_per_xmm + i / 2 + (Lo ? 0 : elements_per_xmm / 2);
+						ret[xmm_i * elements_per_xmm + i] = a[srcI];
+						ret[xmm_i * elements_per_xmm + i + 1] = b[srcI];
+					}
+				}
 				return ret;
 			}
 		};
