4ff1d76a08e092d8a6b5cbe4a5089b1f3620193f
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-12 04:08:53 +0500

    Print vector to screen

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main()
 	if (zcnt < sizeof(buf) - 1) memcpy(&read, buf, std::min(sizeof(read), sizeof(buf)));
 	else read = f32x16(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15);
 
-	std::cout << "Vector "
+	std::cout << "Read as f32x16: " << read << "\n";
 
 
 	system("pause");
