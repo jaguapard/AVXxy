67d8fc1eebbb4ff8ff6cb689588a3295d277a3f2
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-12 09:31:18 +0500

    zmm sized checks for mask_mov

--- include/ISAs/AVX512BW.h
+++ include/ISAs/AVX512BW.h
@@ -14,7 +14,7 @@ namespace AVXXY_NAMESPACE
 			{
 				template<typename S, size_t N>
 				static SIMD_Vector<S, N> eval(op_mask_mov, const SIMD_Vector<S, N>& ifBitClear, const SIMD_BitMask<N>& mask, const SIMD_Vector<S, N>& ifBitSet)
-					requires (sizeof(S) < 4)
+					requires (sizeof(S) < 4 && concepts::zmm_sized<SIMD_Vector<S, N>>)
 				{
 					using namespace concepts;
 					if constexpr (any_i16<S>) return _mm512_mask_mov_epi16(ifBitClear, mask, ifBitSet);
--- include/ISAs/AVX512F.h
+++ include/ISAs/AVX512F.h
@@ -14,7 +14,7 @@ namespace AVXXY_NAMESPACE
 			{
 				template<typename S, size_t N>
 				static SIMD_Vector<S, N> eval(op_mask_mov, const SIMD_Vector<S, N>& ifBitClear, const SIMD_BitMask<N>& mask, const SIMD_Vector<S, N>& ifBitSet)
-					requires (sizeof(S) == 4 || sizeof(S) == 8)
+					requires ((sizeof(S) == 4 || sizeof(S) == 8) && concepts::zmm_sized<SIMD_Vector<S, N>>)
 				{
 					using namespace concepts;
 					if constexpr (is_f64<S>) return _mm512_mask_mov_pd(ifBitClear, mask, ifBitSet);
