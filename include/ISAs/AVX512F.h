3f5179cdd45b69f1a3580336204569feb21ba7a7
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-12 09:17:18 +0500

    AVX512F stub

---
+++ include/ISAs/AVX512F.h
@@ -0,0 +1,19 @@
+#pragma once
+#pragma once
+#include "../namespace.h"
+#include "../tags.h"
+#include "../SIMD_BitMask.h"
+#include "../SIMD_Vector.h"
+namespace AVXXY_NAMESPACE
+{
+	namespace internals
+	{
+		namespace ISA
+		{
+			struct AVX512F
+			{
+
+			};
+		}
+	}
+};
