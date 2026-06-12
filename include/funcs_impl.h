c1fab4c2837a8f9677b5c613b22c0d02a639622d
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-12 05:19:03 +0500

    funcs_impl.h start

---
+++ include/funcs_impl.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "funcs.h"
+
+namespace AVXXY_NAMESPACE
+{
+	template<typename S, size_t N>
+	SIMD_Vector<S, N> AVXXY_NAMESPACE::add(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
+	{
+		return SIMD_Vector<S, N>();
+	}
+}
