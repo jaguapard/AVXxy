bf40cab6d6640e09454e4c9b0b09671845c60d74
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-12 22:46:35 +0500

    AVX512F add, sub

--- include/ISAs/AVX512F.h
+++ include/ISAs/AVX512F.h
@@ -15,6 +15,34 @@ namespace AVXXY_NAMESPACE
 			struct AVX512F
 			{
 				template<typename S, size_t N>
+				static SIMD_Vector<S, N> eval(op_add, const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
+					requires (sizeof(S) >= 4 && sizeof(SIMD_Vector<S, N>) > 32)
+				{
+					using namespace concepts;
+					using T = SIMD_Vector<S, N>;
+					if constexpr (sizeof(T) > 64) return { add(a.lo(), b.lo()), add(a.hi(), b.hi()) };
+					else if constexpr (is_f64<S>) return _mm512_add_pd(a, b);
+					else if constexpr (is_f32<S>) return _mm512_add_ps(a, b);
+					else if constexpr (any_i64<S>) return _mm512_add_epi64(a, b);
+					else if constexpr (any_i32<S>) return _mm512_add_epi32(a, b);
+					else static_assert(always_false_v<S>);
+				}
+
+				template<typename S, size_t N>
+				static SIMD_Vector<S, N> eval(op_sub, const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
+					requires (sizeof(S) >= 4 && sizeof(SIMD_Vector<S, N>) > 32)
+				{
+					using namespace concepts;
+					using T = SIMD_Vector<S, N>;
+					if constexpr (sizeof(T) > 64) return { sub(a.lo(), b.lo()), sub(a.hi(), b.hi()) };
+					else if constexpr (is_f64<S>) return _mm512_sub_pd(a, b);
+					else if constexpr (is_f32<S>) return _mm512_sub_ps(a, b);
+					else if constexpr (any_i64<S>) return _mm512_sub_epi64(a, b);
+					else if constexpr (any_i32<S>) return _mm512_sub_epi32(a, b);
+					else static_assert(always_false_v<S>);
+				}
+
+				template<typename S, size_t N>
 				static SIMD_Vector<S, N> eval(op_mask_mov, const SIMD_Vector<S, N>& ifBitClear, const SIMD_BitMask<N>& mask, const SIMD_Vector<S, N>& ifBitSet)
 					requires (sizeof(S) >= 4 && sizeof(SIMD_Vector<S, N>) > 32)
 				{
