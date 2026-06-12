f2d41e0902191592b7e2d535c3faf4ebdd525cb8
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-12 06:07:23 +0500

    Removed overqualification in funcs_impl

--- include/funcs_impl.h
+++ include/funcs_impl.h
@@ -5,42 +5,42 @@
 namespace AVXXY_NAMESPACE
 {
 	template<typename S, size_t N>
-	inline SIMD_Vector<S, N> AVXXY_NAMESPACE::add(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
+	inline SIMD_Vector<S, N> add(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
 		return internals::DefaultDispatcher::run(internals::op_add{}, a, b);
 	}
 	template<typename S, size_t N>
-	inline SIMD_Vector<S, N> AVXXY_NAMESPACE::sub(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
+	inline SIMD_Vector<S, N> sub(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
 		return internals::DefaultDispatcher::run(internals::op_sub{}, a, b);
 	}
 	template<typename S, size_t N>
-	inline SIMD_Vector<S, N> AVXXY_NAMESPACE::mul(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
+	inline SIMD_Vector<S, N> mul(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
 		return internals::DefaultDispatcher::run(internals::op_mul{}, a, b);
 	}
 	template<typename S, size_t N>
-	inline SIMD_Vector<S, N> AVXXY_NAMESPACE::div(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
+	inline SIMD_Vector<S, N> div(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
 		return internals::DefaultDispatcher::run(internals::op_div{}, a, b);
 	}
 	template<typename S, size_t N>
-	inline SIMD_Vector<S, N> AVXXY_NAMESPACE::logic_and(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
+	inline SIMD_Vector<S, N> logic_and(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
 		return internals::DefaultDispatcher::run(internals::op_and{}, a, b);
 	}
 	template<typename S, size_t N>
-	inline SIMD_Vector<S, N> AVXXY_NAMESPACE::logic_or(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
+	inline SIMD_Vector<S, N> logic_or(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
 		return internals::DefaultDispatcher::run(internals::op_or{}, a, b);
 	}
 	template<typename S, size_t N>
-	inline SIMD_Vector<S, N> AVXXY_NAMESPACE::logic_xor(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
+	inline SIMD_Vector<S, N> logic_xor(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
 		return internals::DefaultDispatcher::run(internals::op_xor{}, a, b);
 	}
 	template<typename S, size_t N>
-	inline SIMD_Vector<S, N> AVXXY_NAMESPACE::logic_not(const SIMD_Vector<S, N>& a)
+	inline SIMD_Vector<S, N> logic_not(const SIMD_Vector<S, N>& a)
 	{
 		return internals::DefaultDispatcher::run(internals::op_not{}, a);
 	}
