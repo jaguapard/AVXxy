08effd07aa43167dda91e913812aab89708ae33c
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-12 09:22:04 +0500

    AVX512F: mask_mov

--- include/ISAs/AVX512F.h
+++ include/ISAs/AVX512F.h
@@ -12,7 +12,16 @@ namespace AVXXY_NAMESPACE
 		{
 			struct AVX512F
 			{
-
+				template<typename S, size_t N>
+				static SIMD_Vector<S, N> eval(op_mask_mov, const SIMD_Vector<S, N>& ifBitClear, const SIMD_BitMask<N>& mask, const SIMD_Vector<S, N>& ifBitSet)
+					requires (sizeof(S) == 4 || sizeof(S) == 8)
+				{
+					using namespace concepts;
+					if constexpr (is_f64<S>) return _mm512_mask_mov_pd(ifBitClear, mask, ifBitSet);
+					else if constexpr (is_f64<S>) return _mm512_mask_mov_ps(ifBitClear, mask, ifBitSet);
+					else if constexpr (any_i64<S>) return _mm512_mask_mov_epi64(ifBitClear, mask, ifBitSet);
+					else if constexpr (any_i32<S>) return _mm512_mask_mov_epi32(ifBitClear, mask, ifBitSet);
+				}
 			};
 		}
 	}
