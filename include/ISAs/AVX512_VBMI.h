88fa3a8d4e20d9939044d8c92ddc870a5a3c16ec
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-13 11:08:46 +0500

    AVX512 vbmi stub

---
+++ include/ISAs/AVX512_VBMI.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "../namespace.h"
+#include "../tags.h"
+#include "../SIMD_BitMask.h"
+#include "../SIMD_Vector.h"
+#include "../FeatureSet.h"
+namespace AVXXY_NAMESPACE
+{
+	namespace internals
+	{
+		namespace ISA
+		{
+			template<internals::FeatureSet FS>
+			struct AVX512VBMI
+			{
+			};
+		}
+	}
+}
