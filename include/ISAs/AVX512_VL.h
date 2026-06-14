fca10c780b3174f9682ff93e3cac6a6c1a7f7508
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-15 02:49:54 +0500

    AVX512 VL abs

--- include/ISAs/AVX512_VL.h
+++ include/ISAs/AVX512_VL.h
@@ -10,9 +10,20 @@ namespace AVXXY_NAMESPACE
 	{
 		namespace ISA
 		{
+			using namespace concepts;
 			template<internals::FeatureSet FS>
 			struct AVX512VL
 			{
+				template<typename S, size_t N>
+					requires (FS.has(AVX512_F) && any_i64<S>)
+				static SIMD_Vector<S, N> eval(op_abs, const SIMD_Vector<S, N>& a)
+				{
+					using namespace concepts;
+					using T = SIMD_Vector<S, N>;
+					if constexpr (ymm_sized<T>) return _mm256_abs_epi64(a);
+					else if constexpr (xmm_sized<T>) return _mm_abs_epi64(a);
+					else static_assert(always_false_v<S>);
+				}
 				template<typename S, size_t N>
 				static SIMD_Vector<S, N> eval(op_mask_mov, const SIMD_Vector<S, N>& ifBitClear, const SIMD_BitMask<N>& mask, const SIMD_Vector<S, N>& ifBitSet)
 					requires (sizeof(S) < 4 && FS.has(AVX512_BW) && (concepts::xmm_sized<SIMD_Vector<S,N>> || concepts::ymm_sized<SIMD_Vector<S,N>>))
