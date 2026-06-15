e5ae06d54c59f59c8fe14d4be235a0b2a647f654
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-16 04:12:41 +0500

    avx512+vl: load

--- include/ISAs/AVX512_F.h
+++ include/ISAs/AVX512_F.h
@@ -819,7 +819,7 @@ namespace AVXXY_NAMESPACE
 					else static_assert(always_false_v<I, S>);
 				}
 				template<typename S, size_t N>
-					requires (sizeof(S) >= 4 && sizeof(SIMD_Vector<S, N>) > 32)
+					requires (sizeof(S) >= 4 && sizeof(SIMD_Vector<S, N>) >= (FS.has(AVX512_VL) ? 0 : 33))
 				static SIMD_Vector<S, N> eval(op_load<S, N>, const void* p, const SIMD_BitMask<N>& mask = SIMD_BitMask<N>::AllOnes, const SIMD_Vector<S, N>& src = 0)
 				{
 					using namespace concepts;
@@ -827,10 +827,18 @@ namespace AVXXY_NAMESPACE
 					SIMD_Vector<S, N> ret;
 					const S* sp = (const S*)p;
 					if constexpr (sizeof(T) > 64) return { load<S,N / 2>(sp,mask.lo(), src.lo()), load<S,N / 2>(sp + N / 2, mask.hi(), src.hi()) };
-					else if constexpr (is_f64<S>) return _mm512_mask_loadu_pd(src, mask, p);
-					else if constexpr (is_f32<S>) return _mm512_mask_loadu_ps(src, mask, p);
-					else if constexpr (any_i64<S>) return _mm512_mask_loadu_epi64(src, mask, p);
-					else if constexpr (any_i32<S>) return _mm512_mask_loadu_epi32(src, mask, p);
+					else if constexpr (zmm_sized<T> && is_f64<S>) return _mm512_mask_loadu_pd(src, mask, p);
+					else if constexpr (zmm_sized<T> && is_f32<S>) return _mm512_mask_loadu_ps(src, mask, p);
+					else if constexpr (zmm_sized<T> && any_i64<S>) return _mm512_mask_loadu_epi64(src, mask, p);
+					else if constexpr (zmm_sized<T> && any_i32<S>) return _mm512_mask_loadu_epi32(src, mask, p);
+					else if constexpr (FS.has(AVX512_VL) && ymm_sized<T> && is_f64<S>) return _mm256_mask_loadu_pd(src, mask, p);
+					else if constexpr (FS.has(AVX512_VL) && ymm_sized<T> && is_f32<S>) return _mm256_mask_loadu_ps(src, mask, p);
+					else if constexpr (FS.has(AVX512_VL) && ymm_sized<T> && any_i64<S>) return _mm256_mask_loadu_epi64(src, mask, p);
+					else if constexpr (FS.has(AVX512_VL) && ymm_sized<T> && any_i32<S>) return _mm256_mask_loadu_epi32(src, mask, p);
+					else if constexpr (FS.has(AVX512_VL) && xmm_sized<T> && is_f64<S>) return _mm_mask_loadu_pd(src, mask, p);
+					else if constexpr (FS.has(AVX512_VL) && xmm_sized<T> && is_f32<S>) return _mm_mask_loadu_ps(src, mask, p);
+					else if constexpr (FS.has(AVX512_VL) && xmm_sized<T> && any_i64<S>) return _mm_mask_loadu_epi64(src, mask, p);
+					else if constexpr (FS.has(AVX512_VL) && xmm_sized<T> && any_i32<S>) return _mm_mask_loadu_epi32(src, mask, p);
 					else static_assert(always_false_v<S>);
 				}
 
