0fda4abe7aac67c72ed4b5329b9fc59adc49634d
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-16 03:54:48 +0500

    AVX512F min/max vl

--- include/ISAs/AVX512_F.h
+++ include/ISAs/AVX512_F.h
@@ -161,31 +161,41 @@ namespace AVXXY_NAMESPACE
 				}
 
 				template<typename S, size_t N>
-					requires (sizeof(S) >= 4 && sizeof(SIMD_Vector<S, N>) > 32)
+					requires ((sizeof(S) >= 4 && sizeof(SIMD_Vector<S, N>) > 32) || (FS.has(AVX512_VL) && any_i64<S>))
 				static SIMD_Vector<S, N> eval(op_min, const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 				{
 					using namespace concepts;
-					if constexpr (sizeof(SIMD_Vector<S, N>) > 64) return { min(a.lo(), b.lo()), min(a.hi(),b.hi()) };
+					using T = SIMD_Vector<S, N>;
+					if constexpr (sizeof(T) > 64) return { min(a.lo(), b.lo()), min(a.hi(),b.hi()) };
 					else if constexpr (is_f64<S>) return _mm512_min_pd(a, b);
 					else if constexpr (is_f32<S>) return _mm512_min_ps(a, b);
-					else if constexpr (is_i64<S>) return _mm512_min_epi64(a, b);
-					else if constexpr (is_u64<S>) return _mm512_min_epu64(a, b);
+					else if constexpr (zmm_sized<T> && is_i64<S>) return _mm512_min_epi64(a, b);
+					else if constexpr (zmm_sized<T> && is_u64<S>) return _mm512_min_epu64(a, b);
+					else if constexpr (FS.has(AVX512_VL) && ymm_sized<T> && is_i64<S>) return _mm256_min_epi64(a, b);
+					else if constexpr (FS.has(AVX512_VL) && ymm_sized<T> && is_u64<S>) return _mm256_min_epu64(a, b);
+					else if constexpr (FS.has(AVX512_VL) && xmm_sized<T> && is_i64<S>) return _mm_min_epi64(a, b);
+					else if constexpr (FS.has(AVX512_VL) && xmm_sized<T> && is_u64<S>) return _mm_min_epu64(a, b);
 					else if constexpr (is_i32<S>) return _mm512_min_epi32(a, b);
 					else if constexpr (is_u32<S>) return _mm512_min_epu32(a, b);
 					else static_assert(always_false_v<S>);
 				}
 				template<typename S, size_t N>
-					requires (sizeof(S) >= 4 && sizeof(SIMD_Vector<S, N>) > 32)
+					requires ((sizeof(S) >= 4 && sizeof(SIMD_Vector<S, N>) > 32) || (FS.has(AVX512_VL) && any_i64<S>))
 				static SIMD_Vector<S, N> eval(op_max, const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 				{
 					using namespace concepts;
-					if constexpr (sizeof(SIMD_Vector<S, N>) > 64) return { max(a.lo(), b.lo()), max(a.hi(),b.hi()) };
+					using T = SIMD_Vector<S, N>;
+					if constexpr (sizeof(T) > 64) return { max(a.lo(), b.lo()), max(a.hi(),b.hi()) };
 					else if constexpr (is_f64<S>) return _mm512_max_pd(a, b);
 					else if constexpr (is_f32<S>) return _mm512_max_ps(a, b);
+					else if constexpr (zmm_sized<T> && is_i64<S>) return _mm512_max_epi64(a, b);
+					else if constexpr (zmm_sized<T> && is_u64<S>) return _mm512_max_epu64(a, b);
+					else if constexpr (FS.has(AVX512_VL) && ymm_sized<T> && is_i64<S>) return _mm256_max_epi64(a, b);
+					else if constexpr (FS.has(AVX512_VL) && ymm_sized<T> && is_u64<S>) return _mm256_max_epu64(a, b);
+					else if constexpr (FS.has(AVX512_VL) && xmm_sized<T> && is_i64<S>) return _mm_max_epi64(a, b);
+					else if constexpr (FS.has(AVX512_VL) && xmm_sized<T> && is_u64<S>) return _mm_max_epu64(a, b);
 					else if constexpr (is_i32<S>) return _mm512_max_epi32(a, b);
 					else if constexpr (is_u32<S>) return _mm512_max_epu32(a, b);
-					else if constexpr (is_i64<S>) return _mm512_max_epi64(a, b);
-					else if constexpr (is_u64<S>) return _mm512_max_epu64(a, b);
 					else static_assert(always_false_v<S>);
 				}
 
