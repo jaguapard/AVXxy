1554eabc96ffd3565ec1279246de02bc9bcf9eee
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-17 16:18:05 +0500

    AVX512-DQ start

--- include/Dispatcher.h
+++ include/Dispatcher.h
@@ -5,6 +5,7 @@
 #include "ISAs/AVX512_F.h"
 #include "ISAs/AVX512_BW.h"
 #include "ISAs/AVX512_VL.h"
+#include "ISAs/AVX512_DQ.h"
 #include "ISAs/AVX512_VBMI.h"
 #include "ISAs/F16C.h"
 
@@ -23,6 +24,7 @@ namespace AVXXY_NAMESPACE
 			static inline constexpr FeatureSet FeatureSet = FS;
 			using order = std::tuple<
 				std::conditional_t<FS.has(AVX512_VBMI), ISA::AVX512VBMI<FS>, Dummy>,
+				std::conditional_t<FS.has(AVX512_DQ), ISA::AVX512DQ<FS>, Dummy>,
 				std::conditional_t<FS.has(AVX512_VL), ISA::AVX512VL<FS>, Dummy>,
 				std::conditional_t<FS.has(AVX512_BW), ISA::AVX512BW<FS>, Dummy>,
 				std::conditional_t<FS.has(AVX512_F), ISA::AVX512F<FS>, Dummy>,
