98acf47bc9d0c039df0ba7931228e5ef998619b4
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-19 16:16:02 +0500

    AVX abs

--- include/ISAs/AVX.h
+++ include/ISAs/AVX.h
@@ -18,7 +18,14 @@ namespace AVXXY_NAMESPACE
 			template<internals::FeatureSet FS>
 			struct AVX
 			{
-
+				template<typename S, size_t N>
+					requires ((is_f32<S> || is_f64<S>) && sizeof(SIMD_Vector<S, N>) > 16)
+				static SIMD_Vector<S, N> eval(op_abs, const SIMD_Vector<S, N>& a)
+				{
+					using U = same_size_uint_t<S>::type;
+					constexpr U sb = U(1) << (sizeof(S) * 8 - 1);
+					return logic_xor(a, std::bit_cast<S>(~sb)); //force sign bit to 0
+				}
 			};
 		}
 	}
