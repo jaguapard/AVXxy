2b2c9aa8b9a6128de58b9a1c0474a3f0af46a448
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-19 20:38:32 +0500

    Removed templating from ISAs and Dispatcher

--- main.cpp
+++ main.cpp
@@ -33,13 +33,13 @@ int main()
 	std::cout << "Read as f32x16: " << read << "\n";
 
 	//This is not how users will use it, but they can if they want to be really specific. This will be dressed in much nicer calls, Dispatcher and tags should be basically invisible to users
-	f32x16 read_x2 = Dispatcher<FS_current>::run(op_add{}, read, read);
+	f32x16 read_x2 = Dispatcher::run(op_add{}, read, read);
 	std::cout << "Read + read as f32x16: " << read_x2 << "\n";
 
 	f32x16 read_x3_add_func = add(read, read_x2);
 	std::cout << "Read * 3 through add func: " << read_x3_add_func << "\n";
 
-	i32x16 cvt = Dispatcher<FS_current>::run(op_cvt<int>{}, read_x2);
+	i32x16 cvt = Dispatcher::run(op_cvt<int>{}, read_x2);
 	std::cout << "Read * 2 as ints: " << cvt << "\n";
 
 	auto div = read / read;
