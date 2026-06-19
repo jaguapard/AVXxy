f60c18d7228e3cefc8634a3e9488dd8084bd6021
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-19 14:26:47 +0500

    AVX2 unpacks

--- include/ISAs/AVX2.h
+++ include/ISAs/AVX2.h
@@ -172,6 +172,31 @@ namespace AVXXY_NAMESPACE
 				}
 
 				template<typename S, size_t N>
+					requires (any_int<S> && sizeof(SIMD_Vector<S, N>) > 16)
+				static SIMD_Vector<S, N> eval(op_unpacklo, const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
+				{
+					using T = SIMD_Vector<S, N>;
+					if constexpr (sizeof(T) > 32) return { unpacklo(a.lo(),b.lo()), unpacklo(a.hi(),b.hi()) };
+					else if constexpr (ymm_sized<T> && any_i64<S>) return _mm256_unpacklo_epi64(a, b);
+					else if constexpr (ymm_sized<T> && any_i32<S>) return _mm256_unpacklo_epi32(a, b);
+					else if constexpr (ymm_sized<T> && any_i16<S>) return _mm256_unpacklo_epi16(a, b);
+					else if constexpr (ymm_sized<T> && any_i8<S>) return _mm256_unpacklo_epi8(a, b);
+					else static_assert(always_false_v<T>);
+				}
+				template<typename S, size_t N>
+					requires (any_int<S> && sizeof(SIMD_Vector<S, N>) > 16)
+				static SIMD_Vector<S, N> eval(op_unpackhi, const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
+				{
+					using T = SIMD_Vector<S, N>;
+					if constexpr (sizeof(T) > 32) return { unpackhi(a.lo(),b.lo()), unpackhi(a.hi(),b.hi()) };
+					else if constexpr (ymm_sized<T> && any_i64<S>) return _mm256_unpackhi_epi64(a, b);
+					else if constexpr (ymm_sized<T> && any_i32<S>) return _mm256_unpackhi_epi32(a, b);
+					else if constexpr (ymm_sized<T> && any_i16<S>) return _mm256_unpackhi_epi16(a, b);
+					else if constexpr (ymm_sized<T> && any_i8<S>) return _mm256_unpackhi_epi8(a, b);
+					else static_assert(always_false_v<T>);
+				}
+
+				template<typename S, size_t N>
 				requires (sizeof(SIMD_Vector<S, N>) > 16 && any_int<S>)
 				static SIMD_BitMask<N> eval(op_cmpeq, const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 				{
