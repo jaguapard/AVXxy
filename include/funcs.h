cd464474ccd622f2a4fa9343b87e98a0a2d8e74f
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-20 19:45:38 +0500

    lower/upper half limitations

--- include/funcs.h
+++ include/funcs.h
@@ -226,8 +226,8 @@ namespace AVXXY_NAMESPACE
 	template <typename S, size_t N> requires (sizeof(S) * 8 >= N)
 		SIMD_Vector<typename concepts::same_size_uint_t<S>::type, N> conflict(const SIMD_Vector<S, N>& a);
 
-	template<typename T> requires (concepts::any_int<T>) typename concepts::bits_to_uint_t<sizeof(T)*4>::type lower_half(const T& a);
-	template<typename T> requires (concepts::any_int<T>) typename concepts::bits_to_uint_t<sizeof(T)*4>::type upper_half(const T& a);
+	template<size_t N, typename T> requires (concepts::any_int<T>) typename concepts::bits_to_uint_t<sizeof(T)*4>::type lower_half(const T& a);
+	template<size_t N, typename T> requires (concepts::any_int<T>) typename concepts::bits_to_uint_t<sizeof(T)*4>::type upper_half(const T& a);
 
 	//Concatenates unsigned integer representations of bit masks and returns the result.
 	//Returned mask size is N1+N2 bits and must be below or equal to 64 bits
