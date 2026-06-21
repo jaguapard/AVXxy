93f596637b9aadd38f62070b5cff48d00e64a40c
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-21 19:17:13 +0500

    SIMD_Mask declarations

--- include/SIMD_Mask.h
+++ include/SIMD_Mask.h
@@ -21,10 +21,40 @@ namespace AVXXY_NAMESPACE
 		requires IsValid_SIMD_Mask<N>
 	class SIMD_Mask
 	{
+	public:
 		template <concepts::LaneSizeEnum FriendS, size_t FriendN>
 			requires IsValid_SIMD_Mask<N>
 		friend class SIMD_Mask;
 
+		using UintT = meta::ScalarSizeTraits<LS>::UintT;
+		using IntT = meta::ScalarSizeTraits<LS>::IntT;
+		using VecT = SIMD_Vector<IntT, N>;
 
+		static inline constexpr bool IsBitMask = internals::FS_current.has(internals::AVX512_F) || !internals::FS_current.has(internals::SSE);
+		static inline constexpr bool IsVectorMask = !IsBitMask;
+		static inline constexpr UintT AllOnesUint = (N == sizeof(UintT) * 8) ? ~UintT(0) : ((UintT(1) << N) - 1);
+		
+		SIMD_Mask() {};
+		SIMD_Mask(UintT bits);
+		SIMD_Mask(const SIMD_Mask<LS, N / 2>& lo, const SIMD_Mask<LS, N / 2>& hi);
+
+		template <concepts::LaneSizeEnum LS2>
+		SIMD_Mask(const SIMD_Mask<LS2, N>& other);
+
+		SIMD_Mask<LS, N / 2> lo() const;
+		SIMD_Mask<LS, N / 2> hi() const;
+
+		SIMD_Mask<LS, N> operator&(const SIMD_Mask<LS, N>& other) const;
+		SIMD_Mask<LS, N> operator|(const SIMD_Mask<LS, N>& other) const;
+		SIMD_Mask<LS, N> operator^(const SIMD_Mask<LS, N>& other) const;
+		SIMD_Mask<LS, N> operator~() const;
+		SIMD_Mask<LS, N>& operator&=(const SIMD_Mask<LS, N>& other);
+		SIMD_Mask<LS, N>& operator|=(const SIMD_Mask<LS, N>& other);
+		SIMD_Mask<LS, N>& operator^=(const SIMD_Mask<LS, N>& other);
+	private:
+		std::conditional_t<IsBitMask, UintT, VecT> underlying;
 	};
+
+	template<typename S, size_t N>
+	using mask_t = SIMD_Mask<meta::ScalarTraits<S>::size_class, N>;
 }
