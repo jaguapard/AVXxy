1913bf631ce47dc6f389e022a586a21968951a15
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-22 15:08:45 +0500

    CRITICAL: fixed mask having wrong uint type

--- include/SIMD_Mask_impl.h
+++ include/SIMD_Mask_impl.h
@@ -6,34 +6,34 @@
 namespace AVXXY_NAMESPACE
 {
 	template<meta::ScalarSizeClassEnum LS, size_t N> requires IsValid_SIMD_Mask<N>
-	inline SIMD_Mask<LS,N>::VecT SIMD_Mask<LS, N>::_movm(UintT bits)
+	inline SIMD_Mask<LS,N>::VecT SIMD_Mask<LS, N>::_movm(BitsUintT bits)
 	{
 		using T = SIMD_Mask<LS, N>::SizeTraits;
 		SIMD_Mask<LS, N>::VecT ret;
 		//scalar movm
 		for (size_t i = 0; i < N; ++i)
 		{
-			ret[i] = bits & (UintT(1) << i) ? T::AllOnesUint : 0;
+			ret[i] = bits & (BitsUintT(1) << i) ? T::AllOnesUint : 0;
 		}
 		return ret;
 		
 	}
 
 	template<meta::ScalarSizeClassEnum LS, size_t N> requires IsValid_SIMD_Mask<N>
-	inline SIMD_Mask<LS,N>::UintT SIMD_Mask<LS, N>::_movemask() const
+	inline SIMD_Mask<LS,N>::BitsUintT SIMD_Mask<LS, N>::_movemask() const
 	{
 		if constexpr (IsBitMask) return underlying & AllOnesUint;
 		//scalar movemask
-		UintT ret = 0;
+		BitsUintT ret = 0;
 		for (size_t i = 0; i < N; ++i)
 		{
-			if (underlying[i] < 0) ret |= UintT(1) << i;
+			if (underlying[i] < 0) ret |= BitsUintT(1) << i;
 		}
 		return ret;
 	}
 
 	template<meta::ScalarSizeClassEnum LS, size_t N> requires IsValid_SIMD_Mask<N>
-	inline SIMD_Mask<LS, N>::SIMD_Mask(UintT bits)
+	inline SIMD_Mask<LS, N>::SIMD_Mask(BitsUintT bits)
 	{
 		if constexpr (IsBitMask) underlying = bits & AllOnesUint;
 		else underlying = SIMD_Mask<LS, N>::_movm(bits);
@@ -42,7 +42,7 @@ namespace AVXXY_NAMESPACE
 	template<meta::ScalarSizeClassEnum LS, size_t N>  requires IsValid_SIMD_Mask<N>
 	inline SIMD_Mask<LS, N>::SIMD_Mask(const SIMD_Mask<LS, N / 2>& lo, const SIMD_Mask<LS, N / 2>& hi)
 	{
-		if constexpr (IsBitMask) return UintT(lo) | (UintT(hi) << (N / 2));
+		if constexpr (IsBitMask) return BitsUintT(lo) | (BitsUintT(hi) << (N / 2));
 		else 
 		{
 			SIMD_Mask<LS, N> ret;
@@ -67,7 +67,7 @@ namespace AVXXY_NAMESPACE
 	}
 
 	template<meta::ScalarSizeClassEnum LS, size_t N> requires IsValid_SIMD_Mask<N>
-	inline SIMD_Mask<LS, N>::operator UintT() const
+	inline SIMD_Mask<LS, N>::operator BitsUintT() const
 	{
 		if constexpr (IsBitMask) return underlying & AllOnesUint;
 		else return this->_movemask();
