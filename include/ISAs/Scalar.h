8e4c7cef28fdb99157fc3a80b581e9d6e4f910db
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-22 20:16:29 +0500

    typename for everybody

--- include/ISAs/Scalar.h
+++ include/ISAs/Scalar.h
@@ -24,7 +24,7 @@ namespace AVXXY_NAMESPACE
 			static SIMD_Vector<typename Op::cvt_to_t, N> eval(const SIMD_Vector<From, N>& a)
 			{
 				scream();
-				using To = Op::cvt_to_t;
+				using To = typename Op::cvt_to_t;
 				SIMD_Vector<To, N> ret;
 				for (size_t i = 0; i < N; ++i) ret[i] = a[i];
 				return ret;
@@ -144,7 +144,7 @@ namespace AVXXY_NAMESPACE
 				return ret;
 			}
 			template<typename Op, typename S, size_t N, typename I>
-				requires (std::same_as<Op, op_shr> && meta::any_int<S> && meta::any_int<I>)
+				requires (std::same_as<Op, op_shr>&& meta::any_int<S>&& meta::any_int<I>)
 			static SIMD_Vector<S, N> eval(const SIMD_Vector<S, N>& a, const SIMD_Vector<I, N>& b)
 			{
 				scream();
@@ -174,7 +174,7 @@ namespace AVXXY_NAMESPACE
 			}
 
 			template<typename Op, typename S, size_t N, typename I>
-				requires (meta::any_int<I> && std::same_as<Op, op_permx>)
+				requires (meta::any_int<I>&& std::same_as<Op, op_permx>)
 			static SIMD_Vector<S, N> eval(const SIMD_Vector<S, N>& a, const SIMD_Vector<I, N>& ind)
 			{
 				scream();
@@ -183,7 +183,7 @@ namespace AVXXY_NAMESPACE
 				return ret;
 			}
 			template<typename Op, typename S, size_t N, typename I>
-				requires (meta::any_int<I> && std::same_as<Op,op_permx2>)
+				requires (meta::any_int<I>&& std::same_as<Op, op_permx2>)
 			static SIMD_Vector<S, N> eval(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b, const SIMD_Vector<I, N>& ind)
 			{
 				scream();
@@ -197,7 +197,7 @@ namespace AVXXY_NAMESPACE
 			}
 
 			template<typename Op, typename S, size_t N>
-				requires (std::is_floating_point_v<S> && std::same_as<Op, op_floor>)
+				requires (std::is_floating_point_v<S>&& std::same_as<Op, op_floor>)
 			static SIMD_Vector<S, N> eval(const SIMD_Vector<S, N>& a)
 			{
 				scream();
@@ -206,7 +206,7 @@ namespace AVXXY_NAMESPACE
 				return ret;
 			}
 			template<typename Op, typename S, size_t N>
-				requires (std::is_floating_point_v<S> && std::same_as<Op, op_ceil>)
+				requires (std::is_floating_point_v<S>&& std::same_as<Op, op_ceil>)
 			static SIMD_Vector<S, N> eval(const SIMD_Vector<S, N>& a)
 			{
 				scream();
@@ -217,11 +217,11 @@ namespace AVXXY_NAMESPACE
 
 
 			template<typename Op>
-			requires meta::IsLoadOp<Op>
-			static SIMD_Vector<Op::S, Op::N> eval(const void* p, const typename SIMD_Vector<Op::S, Op::N>::MaskT& mask, const SIMD_Vector<Op::S, Op::N>& src)
+				requires meta::IsLoadOp<Op>
+			static SIMD_Vector<typename Op::S, Op::N> eval(const void* p, const typename SIMD_Vector<typename Op::S, Op::N>::MaskT& mask, const SIMD_Vector<typename Op::S, Op::N>& src)
 			{
 				scream();
-				using S = Op::S;
+				using S = typename Op::S;
 				constexpr auto N = Op::N;
 
 				SIMD_Vector<S, N> ret;
@@ -230,7 +230,7 @@ namespace AVXXY_NAMESPACE
 				return ret;
 			}
 			template<typename Op, typename S, size_t N>
-			requires (std::same_as<Op, op_store>)
+				requires (std::same_as<Op, op_store>)
 			static void eval(SIMD_Vector<S, N> vec, void* p, const typename SIMD_Vector<S, N>::MaskT& mask)
 			{
 				scream();
@@ -238,17 +238,17 @@ namespace AVXXY_NAMESPACE
 				for (size_t i = 0; i < N; ++i) if (mask[i]) sp[i] = vec[i];
 			}
 			template<typename Op, size_t N, typename I>
-				requires (meta::any_int<I> && meta::IsGatherOp<Op>)
-			static SIMD_Vector<Op::S, N> eval(const void* base, const SIMD_Vector<I, Op::N>& ind, const typename SIMD_Vector<Op::S, Op::N>::MaskT& mask, const SIMD_Vector<Op::S, Op::N>& src = 0)
+				requires (meta::any_int<I>&& meta::IsGatherOp<Op>)
+			static SIMD_Vector<typename Op::S, N> eval(const void* base, const SIMD_Vector<I, Op::N>& ind, const typename SIMD_Vector<typename Op::S, Op::N>::MaskT& mask, const SIMD_Vector<typename Op::S, Op::N>& src = 0)
 			{
 				scream();
-				SIMD_Vector<Op::S, N> ret;
+				SIMD_Vector<typename Op::S, N> ret;
 				size_t addr = size_t(base);
 				for (size_t i = 0; i < Op::N; ++i) ret[i] = mask[i] ? *(const Op::S*)(addr + Op::Scale * ind[i]) : src[i];
 				return ret;
 			}
 			template<typename Op, typename S, size_t N, typename I>
-				requires (meta::any_int<I> && meta::IsScatterOp<Op>)
+				requires (meta::any_int<I>&& meta::IsScatterOp<Op>)
 			static void eval(const SIMD_Vector<S, N>& v, void* base, const SIMD_Vector<I, N>& ind, const typename SIMD_Vector<S, N>::MaskT& mask)
 			{
 				scream();
@@ -258,7 +258,7 @@ namespace AVXXY_NAMESPACE
 
 
 			template<typename Op, typename S, size_t N>
-			requires (std::same_as<Op,op_compress>)
+				requires (std::same_as<Op, op_compress>)
 			static SIMD_Vector<S, N> eval(const typename SIMD_Vector<S, N>::MaskT& mask, const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& src = 0)
 			{
 				scream();
@@ -380,22 +380,22 @@ namespace AVXXY_NAMESPACE
 			}
 
 			template<typename Op, size_t N, meta::ScalarSizeClassEnum C>
-			requires (meta::IsMovmOp<Op>)
-			static SIMD_Vector<Op::S, N> eval(const SIMD_Mask<C, N>& mask)
+				requires (meta::IsMovmOp<Op>)
+			static SIMD_Vector<typename Op::S, N> eval(const SIMD_Mask<C, N>& mask)
 			{
 				scream();
-				SIMD_Vector<Op::S, N> ret;
-				using Tr = meta::ScalarTraits<Op::S>;
+				SIMD_Vector<typename Op::S, N> ret;
+				using Tr = meta::ScalarTraits<typename Op::S>;
 				for (size_t i = 0; i < N; ++i)
 				{
 					typename Tr::UintT u = mask[i] ? Tr::AllOnesUint : 0;
-					ret[i] = std::bit_cast<Op::S>(u);
+					ret[i] = std::bit_cast<typename Op::S>(u);
 				}
 				return ret;
 			}
-			
+
 			template<typename Op, typename S, size_t N>
-			requires (std::same_as<Op, op_movemask>)
+				requires (std::same_as<Op, op_movemask>)
 			static mask_t<S, N> eval(const SIMD_Vector<S, N>& vec)
 			{
 				mask_t<S, N> ret;
@@ -427,9 +427,9 @@ namespace AVXXY_NAMESPACE
 				}
 				return ret;
 			}
-			
-				//scream your lungs out if scalar fallback is reached and this function is enabled via AVXXY_NOISY_SCALAR define
-				static void scream(std::source_location loc = std::source_location::current())
+
+			//scream your lungs out if scalar fallback is reached and this function is enabled via AVXXY_NOISY_SCALAR define
+			static void scream(std::source_location loc = std::source_location::current())
 			{
 #ifdef AVXXY_NOISY_SCALAR
 				std::cout << "\nScalar fallback reached:" << loc.function_name() << "\n";
