4e025249d80e3d80d34111800f70b388e2900e36
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-22 20:05:24 +0500

    Fixed up funcs

--- include/funcs_impl.h
+++ include/funcs_impl.h
@@ -10,72 +10,72 @@ namespace AVXXY_NAMESPACE
 	{
 		return internals::Dispatcher::run<internals::op_add>(a, b);
 	}
-	/*
+
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> sub(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
-		return internals::op_sub::eval(a, b);
+		return internals::Dispatcher::run<internals::op_sub>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> mul(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
-		return internals::op_mul::eval(a, b);
+		return internals::Dispatcher::run<internals::op_mul>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> div(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
-		return internals::op_div::eval(a, b);
+		return internals::Dispatcher::run<internals::op_div>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> logic_and(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
-		return internals::op_and::eval(a, b);
+		return internals::Dispatcher::run<internals::op_and>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> logic_or(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
-		return internals::op_or::eval(a, b);
+		return internals::Dispatcher::run<internals::op_or>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> logic_xor(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
-		return internals::op_xor::eval(a, b);
+		return internals::Dispatcher::run<internals::op_xor>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> logic_not(const SIMD_Vector<S, N>& a)
 	{
-		return internals::op_not::eval(a);
+		return internals::Dispatcher::run<internals::op_not>(a);
 	}
 	template<typename S, size_t N, typename I>
 	__forceinline SIMD_Vector<S, N> shift_left(const SIMD_Vector<S, N>& a, const SIMD_Vector<I, N>& amount)
 	{
-		return internals::op_shl::eval(a, amount);
+		return internals::Dispatcher::run<internals::op_shl>(a, amount);
 	}
 	template<typename S, size_t N, typename I>
 	__forceinline SIMD_Vector<S, N> shift_right(const SIMD_Vector<S, N>& a, const SIMD_Vector<I, N>& amount)
 	{
-		return internals::op_shr::eval(a, amount);
+		return internals::Dispatcher::run<internals::op_shr>(a, amount);
 	}
 	template<typename S, size_t N, typename I>
 	__forceinline SIMD_Vector<S, N> permx(const SIMD_Vector<S, N>& a, const SIMD_Vector<I, N>& ind)
 	{
-		return internals::op_permx::eval(a, ind);
+		return internals::Dispatcher::run<internals::op_permx>(a, ind);
 	}
 	template<typename S, size_t N, typename I>
 	__forceinline SIMD_Vector<S, N> permx2(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b, const SIMD_Vector<I, N>& ind)
 	{
-		return internals::op_permx2::eval(a, b, ind);
+		return internals::Dispatcher::run<internals::op_permx2>(a, b, ind);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<float, N> sqrtf(const SIMD_Vector<S, N>& a)
 	{
-		return internals::op_sqrtf::eval(a);
+		return internals::Dispatcher::run<internals::op_sqrtf>(a);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<double, N> sqrtd(const SIMD_Vector<S, N>& a)
 	{
-		return internals::op_sqrtd::eval(a);
-	}*/
+		return internals::Dispatcher::run<internals::op_sqrtd>(a);
+	}
 
 
 	template<typename To, size_t N, typename From>
@@ -83,7 +83,7 @@ namespace AVXXY_NAMESPACE
 	{
 		return internals::Dispatcher::run<internals::op_cvt<To>>(value);
 	}
-	/*
+
 	template<typename T, typename S, size_t N>
 		requires (meta::IsSimdVector<T>)
 	__forceinline T vcast(const SIMD_Vector<S, N>& value)
@@ -102,7 +102,7 @@ namespace AVXXY_NAMESPACE
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> mask_mov(const SIMD_Vector<S, N>& ifBitClear, const mask_t<S, N>& mask, const SIMD_Vector<S, N>& ifBitSet)
 	{
-		return internals::op_mask_mov::eval(ifBitClear, mask, ifBitSet);
+		return internals::Dispatcher::run<internals::op_mask_mov>(ifBitClear, mask, ifBitSet);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> maskz_mov(const mask_t<S, N>& mask, const SIMD_Vector<S, N>& ifBitSet)
@@ -117,78 +117,78 @@ namespace AVXXY_NAMESPACE
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> load(const void* p, const mask_t<S, N>& mask, const SIMD_Vector<S, N>& src)
 	{
-		return internals::op_load::eval<S, N>(p, mask, src);
+		return internals::Dispatcher::run<internals::op_load<S, N>>(p, mask, src);
 	}
 	template<typename S, size_t N>
 	__forceinline void store(const SIMD_Vector<S, N>& v, void* p, const mask_t<S, N>& mask)
 	{
-		return internals::op_store::eval(v, p, mask);
+		return internals::Dispatcher::run<internals::op_store>(v, p, mask);
 	}
 
 	template<typename S, size_t N, size_t Scale, typename I>
 	__forceinline void scatter(const SIMD_Vector<S, N>& vec, void* base, const SIMD_Vector<I, N>& ind, const mask_t<S, N>& mask)
 	{
-		return internals::op_scatter::eval<S, N, Scale, I>(vec, base, ind, mask);
+		return internals::Dispatcher::run<internals::op_scatter<Scale>>(vec, base, ind, mask);
 	}
 	template<typename S, size_t N>
 	__forceinline mask_t<S, N> cmp_equal(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
-		return internals::op_cmpeq::eval(a, b);
+		return internals::Dispatcher::run<internals::op_cmpeq>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline mask_t<S, N> cmp_not_equal(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
-		return internals::op_cmpneq::eval(a, b);
+		return internals::Dispatcher::run<internals::op_cmpneq>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline mask_t<S, N> cmp_less(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
-		return internals::op_cmplt::eval(a, b);
+		return internals::Dispatcher::run<internals::op_cmplt>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline mask_t<S, N> cmp_less_or_equal(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
-		return internals::op_cmple::eval(a, b);
+		return internals::Dispatcher::run<internals::op_cmple>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline mask_t<S, N> cmp_greater(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
-		return internals::op_cmpgt::eval(a, b);
+		return internals::Dispatcher::run<internals::op_cmpgt>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline mask_t<S, N> cmp_greater_or_equal(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
-		return internals::op_cmpge::eval(a, b);
+		return internals::Dispatcher::run<internals::op_cmpge>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> abs(const SIMD_Vector<S, N>& a)
 	{
-		return internals::op_abs::eval(a);
+		return internals::Dispatcher::run<internals::op_abs>(a);
 	}
 
 	template<typename S, size_t N>
 		requires (meta::any_float<S>)
 	__forceinline SIMD_Vector<S, N> floor(const SIMD_Vector<S, N>& a)
 	{
-		return internals::op_floor::eval(a);
+		return internals::Dispatcher::run<internals::op_floor>(a);
 	}
 	template<typename S, size_t N>
 		requires (meta::any_float<S>)
 	__forceinline SIMD_Vector<S, N> ceil(const SIMD_Vector<S, N>& a)
 	{
-		return internals::op_ceil::eval(a);
+		return internals::Dispatcher::run<internals::op_ceil>(a);
 	}
 
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> min(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
-		return internals::op_min::eval(a, b);
+		return internals::Dispatcher::run<internals::op_min>(a, b);
 	}
 
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> max(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
-		return internals::op_max::eval(a, b);
+		return internals::Dispatcher::run<internals::op_max>(a, b);
 	}
 
 	template<typename S, size_t N>
@@ -201,40 +201,40 @@ namespace AVXXY_NAMESPACE
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> unpacklo(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
-		return internals::op_unpacklo::eval(a, b);
+		return internals::Dispatcher::run<internals::op_unpacklo>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> unpackhi(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
-		return internals::op_unpackhi::eval(a, b);
+		return internals::Dispatcher::run<internals::op_unpackhi>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> compress(const mask_t<S, N>& mask, const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& src)
 	{
-		return internals::op_compress::eval(mask, a, src);
+		return internals::Dispatcher::run<internals::op_compress>(mask, a, src);
 	}
 	template<typename S, size_t N>
 		requires (sizeof(S) * 8 >= N)
 	__forceinline SIMD_Vector<typename meta::ScalarTraits<S>::UintT, N> conflict(const SIMD_Vector<S, N>& a)
 	{
-		return internals::op_conflict::eval(a);
+		return internals::Dispatcher::run<internals::op_conflict>(a);
 	}
 
 	template<typename S, size_t N>
 	mask_t<S, N> movemask(const SIMD_Vector<S, N>& v)
 	{
-		return internals::op_movemask::eval(v);
+		return internals::Dispatcher::run<internals::op_movemask>(v);
 	}
 
 	template<typename S, size_t N, meta::ScalarSizeClassEnum C>
 	SIMD_Vector<S, N> movm(const SIMD_Mask<C, N>& mask)
 	{
-		return internals::op_movm::eval<S, N>(mask);
+		return internals::Dispatcher::run<internals::op_movm<S>>(mask);
 	}
 
 	template<typename S, size_t N, size_t Scale, typename I>
 	__forceinline SIMD_Vector<S, N> __gather_impl(const void* base, const SIMD_Vector<I, N>& ind, const typename SIMD_Vector<S, N>::MaskT& mask, const SIMD_Vector<S, N>& src)
 	{
-		return internals::op_gather::eval<S, N, Scale>(base, ind, mask, src);
-	}*/
+		return internals::Dispatcher::run<internals::op_gather<S, N, Scale>>(base, ind, mask, src);
+	}
 }
