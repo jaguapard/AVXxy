25002b0d3276aaf69afc4218a49ab17597869683
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-23 21:56:13 +0500

    Added casting to unsigned integer types and back to small FP types in funcs_impl (backends won't have to convern it)

--- include/funcs_impl.h
+++ include/funcs_impl.h
@@ -5,9 +5,7 @@
 
 namespace AVXXY_NAMESPACE
 {
-	using namespace meta;
-	using U = typename ScalarTraits<S>::UintT;
-#define AVXXY_RUN(op) internals::Dispatcher::run<internals::op>
+//#define AVXXY_RUN(op) internals::Dispatcher::run<internals::op>
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> add(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
@@ -43,28 +41,31 @@ namespace AVXXY_NAMESPACE
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
 		if constexpr (!is_f32<S> && !is_f64<S> && !any_int<S>) return vcast<S>(logic_and(vcast<U>(a), vcast<U>(b)));
-		return internals::Dispatcher::run<internals::op_and>(a, b);
+		else return internals::Dispatcher::run<internals::op_and>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> logic_or(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
-		return internals::Dispatcher::run<internals::op_or>(a, b);
+		if constexpr (!is_f32<S> && !is_f64<S> && !any_int<S>) return vcast<S>(logic_or(vcast<U>(a), vcast<U>(b)));
+		else return internals::Dispatcher::run<internals::op_or>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> logic_xor(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
-		return internals::Dispatcher::run<internals::op_xor>(a, b);
+		if constexpr (!is_f32<S> && !is_f64<S> && !any_int<S>) return vcast<S>(logic_xor(vcast<U>(a), vcast<U>(b)));
+		else return internals::Dispatcher::run<internals::op_xor>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> logic_not(const SIMD_Vector<S, N>& a)
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
-		return internals::Dispatcher::run<internals::op_not>(a);
+		if constexpr (!is_f32<S> && !is_f64<S> && !any_int<S>) return vcast<S>(logic_not(vcast<U>(a)));
+		else return internals::Dispatcher::run<internals::op_not>(a);
 	}
 	template<typename S, size_t N, typename I>
 	__forceinline SIMD_Vector<S, N> shift_left(const SIMD_Vector<S, N>& a, const SIMD_Vector<I, N>& amount)
@@ -85,14 +86,16 @@ namespace AVXXY_NAMESPACE
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
-		return internals::Dispatcher::run<internals::op_permx>(a, ind);
+		if constexpr (!is_f32<S> && !is_f64<S> && !any_int<S>) return vcast<S>(permx(vcast<U>(a), ind));
+		else return internals::Dispatcher::run<internals::op_permx>(a, ind);
 	}
 	template<typename S, size_t N, typename I>
 	__forceinline SIMD_Vector<S, N> permx2(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b, const SIMD_Vector<I, N>& ind)
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
-		return internals::Dispatcher::run<internals::op_permx2>(a, b, ind);
+		if constexpr (!is_f32<S> && !is_f64<S> && !any_int<S>) return vcast<S>(permx2(vcast<U>(a), vcast<U>(b), ind));
+		else return internals::Dispatcher::run<internals::op_permx2>(a, b, ind);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<float, N> sqrtf(const SIMD_Vector<S, N>& a)
@@ -114,13 +117,12 @@ namespace AVXXY_NAMESPACE
 	__forceinline SIMD_Vector<To, N> vcvt(const SIMD_Vector<From, N>& value)
 	{
 		using namespace meta;
-		using U = typename ScalarTraits<S>::UintT;
 		if constexpr ((meta::is_fp16<From> && !meta::is_f32<To>) || (!meta::is_f32<From> && meta::is_fp16<To>)) return vcvt<To>(vcvt<float>(value));
 		else return internals::Dispatcher::run<internals::op_cvt<To>>(value);
 	}
 
 	template<typename S2, typename S, size_t N> requires (meta::IsScalarType<S2> && (sizeof(SIMD_Vector<S, N>) % sizeof(S2) == 0))
-		SIMD_Vector<S2, sizeof(SIMD_Vector<S, N>) / sizeof(S2)> vcast(const SIMD_Vector<S, N>& a)
+	__forceinline SIMD_Vector<S2, sizeof(SIMD_Vector<S, N>) / sizeof(S2)> vcast(const SIMD_Vector<S, N>& a)
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
@@ -128,7 +130,7 @@ namespace AVXXY_NAMESPACE
 	}
 	template<typename T, typename S, size_t N>
 		requires (meta::IsSimdVector<T> && (sizeof(SIMD_Vector<S, N>) % sizeof(typename T::ScalarT) == 0) && sizeof(SIMD_Vector<S, N>) == sizeof(T))
-	T vcast(const SIMD_Vector<S, N>& a)
+	__forceinline T vcast(const SIMD_Vector<S, N>& a)
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
@@ -137,14 +139,14 @@ namespace AVXXY_NAMESPACE
 
 
 	template<typename T, typename S, size_t N> requires (sizeof(T) == sizeof(SIMD_Vector<S, N>))
-		__forceinline T vreinterpret(const SIMD_Vector<S, N>& value)
+	__forceinline T vreinterpret(const SIMD_Vector<S, N>& value)
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
 		return vreinterpret_us<T>(value);
 	}
 	template<typename T, typename S, size_t N>
-	T vreinterpret_us(const SIMD_Vector<S, N>& value)
+	__forceinline T vreinterpret_us(const SIMD_Vector<S, N>& value)
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
@@ -157,7 +159,8 @@ namespace AVXXY_NAMESPACE
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
-		return internals::Dispatcher::run<internals::op_mask_mov>(ifBitClear, mask, ifBitSet);
+		if constexpr (!is_f32<S> && !is_f64<S> && !any_int<S>) return vcast<S>(mask_mov(vcast<U>(ifBitClear), mask, vcast<U>(ifBitSet)));
+		else return internals::Dispatcher::run<internals::op_mask_mov>(ifBitClear, mask, ifBitSet);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> maskz_mov(const mask_t<S, N>& mask, const SIMD_Vector<S, N>& ifBitSet)
@@ -178,7 +181,7 @@ namespace AVXXY_NAMESPACE
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
-		if constexpr (!meta::is_f64<S> && !meta::is_f32<S>) return SIMD_Vector<S, N>::from_bits_us(internals::Dispatcher::run<internals::op_load<typename meta::ScalarTraits<S>::UintT, N>>(p, mask, src));
+		if constexpr (!is_f32<S> && !is_f64<S> && !any_int<S>) return vcast<S>(load<S, N>(p, mask, vcast<U>(src)));
 		else return internals::Dispatcher::run<internals::op_load<S, N>>(p, mask, src);
 	}
 	template<typename S, size_t N>
@@ -186,7 +189,8 @@ namespace AVXXY_NAMESPACE
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
-		internals::Dispatcher::run<internals::op_store>(v, p, mask);
+		if constexpr (!is_f32<S> && !is_f64<S> && !any_int<S>) store(vcast<U>(v), p, mask);
+		else internals::Dispatcher::run<internals::op_store>(v, p, mask);
 	}
 
 	template<typename S, size_t N, size_t Scale, typename I>
@@ -194,7 +198,8 @@ namespace AVXXY_NAMESPACE
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
-		internals::Dispatcher::run<internals::op_scatter<Scale>>(vec, base, ind, mask);
+		if constexpr (!is_f32<S> && !is_f64<S> && !any_int<S>) scatter<S, N, Scale, I>(vcast<U>(vec), base, ind, mask);
+		else internals::Dispatcher::run<internals::op_scatter<Scale>>(vec, base, ind, mask);
 	}
 	template<typename S, size_t N>
 	__forceinline mask_t<S, N> cmp_equal(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
@@ -293,21 +298,24 @@ namespace AVXXY_NAMESPACE
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
-		return internals::Dispatcher::run<internals::op_unpacklo>(a, b);
+		if constexpr (!is_f32<S> && !is_f64<S> && !any_int<S>) return vcast<S>(unpacklo(vcast<U>(a), vcast<U>(b)));
+		else return internals::Dispatcher::run<internals::op_unpacklo>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> unpackhi(const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& b)
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
-		return internals::Dispatcher::run<internals::op_unpackhi>(a, b);
+		if constexpr (!is_f32<S> && !is_f64<S> && !any_int<S>) return vcast<S>(unpackhi(vcast<U>(a), vcast<U>(b)));
+		else return internals::Dispatcher::run<internals::op_unpackhi>(a, b);
 	}
 	template<typename S, size_t N>
 	__forceinline SIMD_Vector<S, N> compress(const mask_t<S, N>& mask, const SIMD_Vector<S, N>& a, const SIMD_Vector<S, N>& src)
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
-		return internals::Dispatcher::run<internals::op_compress>(mask, a, src);
+		if constexpr (!is_f32<S> && !is_f64<S> && !any_int<S>) return vcast<S>(compress(mask, vcast<U>(a), vcast<U>(src)));
+		else return internals::Dispatcher::run<internals::op_compress>(mask, a, src);
 	}
 	template<typename S, size_t N>
 		requires (sizeof(S) * 8 >= N)
@@ -315,22 +323,25 @@ namespace AVXXY_NAMESPACE
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
+		//TODO: allow bigger CD?
 		return internals::Dispatcher::run<internals::op_conflict>(a);
 	}
 
 	template<typename S, size_t N>
-	mask_t<S, N> movemask(const SIMD_Vector<S, N>& v)
+	__forceinline mask_t<S, N> movemask(const SIMD_Vector<S, N>& v)
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
-		return internals::Dispatcher::run<internals::op_movemask>(v);
+		if constexpr (!is_f32<S> && !is_f64<S> && !any_int<S>) return movemask(vcast<U>(v));
+		else return internals::Dispatcher::run<internals::op_movemask>(v);
 	}
 
 	template<typename S, meta::ScalarSizeClassEnum C, size_t N>
-	SIMD_Vector<S, N> movm(const SIMD_Mask<C, N>& mask)
+	__forceinline SIMD_Vector<S, N> movm(const SIMD_Mask<C, N>& mask)
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
+		if constexpr (!is_f32<S> && !is_f64<S> && !any_int<S>) return vcast<S>(movm<U>(mask));
 		return internals::Dispatcher::run<internals::op_movm<S>>(mask);
 	}
 
@@ -339,7 +350,7 @@ namespace AVXXY_NAMESPACE
 	{
 		using namespace meta;
 		using U = typename ScalarTraits<S>::UintT;
-		return internals::Dispatcher::run<internals::op_gather<S, N, Scale>>(base, ind, mask, src);
+		if constexpr (!is_f32<S> && !is_f64<S> && !any_int<S>) return vcast<S>(__gather_impl<U, N, Scale, I>(base, ind, mask, vcast<U>(src)));
+		else return internals::Dispatcher::run<internals::op_gather<S, N, Scale>>(base, ind, mask, src);
 	}
-#undef AVXXY_RUN
 }
