7b2bc312e27bdf3fef47506fe42c7cf32dc62101
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-25 03:33:03 +0500

    cmpeq

--- include/funcs_impl.h
+++ include/funcs_impl.h
@@ -985,8 +985,7 @@ namespace AVXXY_NAMESPACE
 		using U = typename ScalarTraits<S>::UintT;
 		using T = SIMD_Vector<S, N>;
 
-		if constexpr (sizeof(T) > 64) return { cmp_equal(a.lo(),b.lo()), cmp_equal(a.hi(),b.hi()) };
-		else if constexpr (FS.has(AVX512_BW) && zmm_sized<T> && is_i8<S>) return _mm512_cmpeq_epi8_mask(a, b);
+		if constexpr (FS.has(AVX512_BW) && zmm_sized<T> && is_i8<S>) return _mm512_cmpeq_epi8_mask(a, b);
 		else if constexpr (FS.has(AVX512_BW) && zmm_sized<T> && is_u8<S>) return _mm512_cmpeq_epu8_mask(a, b);
 		else if constexpr (FS.has(AVX512_BW) && zmm_sized<T> && is_i16<S>) return _mm512_cmpeq_epi16_mask(a, b);
 		else if constexpr (FS.has(AVX512_BW) && zmm_sized<T> && is_u16<S>) return _mm512_cmpeq_epu16_mask(a, b);
@@ -996,7 +995,7 @@ namespace AVXXY_NAMESPACE
 		else if constexpr (FS.has(AVX512_F) && zmm_sized<T> && is_u64<S>) return _mm512_cmpeq_epu64_mask(a, b);
 		else if constexpr (FS.has(AVX512_F) && zmm_sized<T> && is_f32<S>) return _mm512_cmp_ps_mask(a, b, _CMP_EQ_OQ);
 		else if constexpr (FS.has(AVX512_F) && zmm_sized<T> && is_f64<S>) return _mm512_cmp_pd_mask(a, b, _CMP_EQ_OQ);
-		else if constexpr (sizeof(T) > 32) return { cmp_equal(a.lo(),b.lo()), cmp_equal(a.hi(),b.hi()) };
+
 		else if constexpr (FS.has(AVX512_VL) && FS.has(AVX512_BW) && ymm_sized<T> && is_i8<S>) return _mm256_cmpeq_epi8_mask(a, b);
 		else if constexpr (FS.has(AVX512_VL) && FS.has(AVX512_BW) && ymm_sized<T> && is_u8<S>) return _mm256_cmpeq_epu8_mask(a, b);
 		else if constexpr (FS.has(AVX512_VL) && FS.has(AVX512_BW) && ymm_sized<T> && is_i16<S>) return _mm256_cmpeq_epi16_mask(a, b);
@@ -1007,8 +1006,7 @@ namespace AVXXY_NAMESPACE
 		else if constexpr (FS.has(AVX512_VL) && FS.has(AVX512_F) && ymm_sized<T> && is_u64<S>) return _mm256_cmpeq_epu64_mask(a, b);
 		else if constexpr (FS.has(AVX512_VL) && FS.has(AVX512_F) && ymm_sized<T> && is_f32<S>) return _mm256_cmp_ps_mask(a, b, _CMP_EQ_OQ);
 		else if constexpr (FS.has(AVX512_VL) && FS.has(AVX512_F) && ymm_sized<T> && is_f64<S>) return _mm256_cmp_pd_mask(a, b, _CMP_EQ_OQ);
-		//TODO: add AVX2 and AVX!
-		//else if constexpr (FS.has(AVX2) && )
+
 		else if constexpr (sizeof(T) > 16) return { cmp_equal(a.lo(),b.lo()), cmp_equal(a.hi(),b.hi()) };
 		else if constexpr (FS.has(AVX512_VL) && FS.has(AVX512_BW) && xmm_sized<T> && is_i8<S>) return _mm_cmpeq_epi8_mask(a, b);
 		else if constexpr (FS.has(AVX512_VL) && FS.has(AVX512_BW) && xmm_sized<T> && is_u8<S>) return _mm_cmpeq_epu8_mask(a, b);
@@ -1020,7 +1018,22 @@ namespace AVXXY_NAMESPACE
 		else if constexpr (FS.has(AVX512_VL) && FS.has(AVX512_F) && xmm_sized<T> && is_u64<S>) return _mm_cmpeq_epu64_mask(a, b);
 		else if constexpr (FS.has(AVX512_VL) && FS.has(AVX512_F) && xmm_sized<T> && is_f32<S>) return _mm_cmp_ps_mask(a, b, _CMP_EQ_OQ);
 		else if constexpr (FS.has(AVX512_VL) && FS.has(AVX512_F) && xmm_sized<T> && is_f64<S>) return _mm_cmp_pd_mask(a, b, _CMP_EQ_OQ);
-		//TODO: add SSE
+
+		else if constexpr (FS.has(AVX2) && ymm_sized<T> && any_i64<S>) return _mm256_cmpeq_epi64(a, b);
+		else if constexpr (FS.has(AVX2) && ymm_sized<T> && any_i32<S>) return _mm256_cmpeq_epi32(a, b);
+		else if constexpr (FS.has(AVX2) && ymm_sized<T> && any_i16<S>) return _mm256_cmpeq_epi16(a, b);
+		else if constexpr (FS.has(AVX2) && ymm_sized<T> && any_i8<S>) return _mm256_cmpeq_epi8(a, b);
+		else if constexpr (FS.has(AVX) && ymm_sized<T> && is_f64<S>) return _mm256_cmp_pd(a, b, _CMP_EQ_OQ);
+		else if constexpr (FS.has(AVX) && ymm_sized<T> && is_f32<S>) return _mm256_cmp_ps(a, b, _CMP_EQ_OQ);
+
+		else if constexpr (FS.has(SSE41) && xmm_sized<T> && any_i64<S>) return _mm_cmpeq_epi64(a, b);
+		else if constexpr (FS.has(SSE2) && xmm_sized<T> && any_i32<S>) return _mm_cmpeq_epi32(a, b);
+		else if constexpr (FS.has(SSE2) && xmm_sized<T> && any_i16<S>) return _mm_cmpeq_epi16(a, b);
+		else if constexpr (FS.has(SSE2) && xmm_sized<T> && any_i8<S>) return _mm_cmpeq_epi8(a, b);
+		else if constexpr (FS.has(SSE2) && xmm_sized<T> && is_f64<S>) return _mm_cmpeq_pd(a, b);
+		else if constexpr (FS.has(SSE) && xmm_sized<T> && is_f32<S>) return _mm_cmpeq_ps(a, b);
+
+		else if constexpr (sizeof(T) > 16) return { cmp_equal(a.lo(),b.lo()), cmp_equal(a.hi(),b.hi()) };
 		else
 		{
 			internals::scream();
