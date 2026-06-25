a03b491ab39e1b73bf75db1d176371fd1aa0c6f1
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-25 19:07:07 +0500

    min

--- include/funcs_impl.h
+++ include/funcs_impl.h
@@ -1588,14 +1588,33 @@ namespace AVXXY_NAMESPACE
 		else if constexpr (FS.has(AVX512_F) && FS.has(AVX512_VL) && xmm_sized<T> && is_i64<S>) return _mm_min_epi64(a, b);
 		else if constexpr (FS.has(AVX512_F) && FS.has(AVX512_VL) && xmm_sized<T> && is_u64<S>) return _mm_min_epu64(a, b);
 
+		else if constexpr (FS.has(AVX2) && ymm_sized<T> && is_i8<S>) return _mm256_min_epi8(a, b);
+		else if constexpr (FS.has(AVX2) && ymm_sized<T> && is_i16<S>) return _mm256_min_epi16(a, b);
+		else if constexpr (FS.has(AVX2) && ymm_sized<T> && is_i32<S>) return _mm256_min_epi32(a, b);
+		else if constexpr (FS.has(AVX2) && ymm_sized<T> && is_u8<S>) return _mm256_min_epu8(a, b);
+		else if constexpr (FS.has(AVX2) && ymm_sized<T> && is_u16<S>) return _mm256_min_epu16(a, b);
+		else if constexpr (FS.has(AVX2) && ymm_sized<T> && is_u32<S>) return _mm256_min_epu32(a, b);
+		else if constexpr (FS.has(AVX) && ymm_sized<T> && is_f64<S>) return _mm256_min_pd(a, b);
+		else if constexpr (FS.has(AVX) && ymm_sized<T> && is_f32<S>) return _mm256_min_ps(a, b);
+
+		else if constexpr (FS.has(SSE41) && xmm_sized<T> && is_i8<S>) return _mm_min_epi8(a, b);
+		else if constexpr (FS.has(SSE2) && xmm_sized<T> && is_i16<S>) return _mm_min_epi16(a, b);
+		else if constexpr (FS.has(SSE41) && xmm_sized<T> && is_i32<S>) return _mm_min_epi32(a, b);
+		else if constexpr (FS.has(SSE2) && xmm_sized<T> && is_u8<S>) return _mm_min_epu8(a, b);
+		else if constexpr (FS.has(SSE41) && xmm_sized<T> && is_u16<S>) return _mm_min_epu16(a, b);
+		else if constexpr (FS.has(SSE41) && xmm_sized<T> && is_u32<S>) return _mm_min_epu32(a, b);
+
 		else if constexpr (sizeof(T) > 16) return T{ min(a.lo(), b.lo()), min(a.hi(),b.hi()) };
+		else return mask_mov(a, b < a, b);
+
+		/*
 		else
 		{
 			internals::scream();
 			SIMD_Vector<S, N> ret;
 			for (size_t i = 0; i < N; ++i) ret[i] = std::min(a[i], b[i]);
 			return ret;
-		}
+		}*/
 	}
 
 	template<typename S, size_t N>
