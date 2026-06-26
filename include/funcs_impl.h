d0ce2ada49b2c79724c27bc2654726df8f1053be
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-26 19:56:43 +0500

    prettified AVX2 epi16->epi8 narrowing

--- include/funcs_impl.h
+++ include/funcs_impl.h
@@ -764,7 +764,7 @@ namespace AVXXY_NAMESPACE
 		//TODO: put it in proper place!
 		else if constexpr (FS.has(AVX2) && is_ymm_size(MaxSize) && any_i16<From> && any_i8<To>)
 		{
-			__m256i sh = _mm256_shuffle_epi8(a, _mm256_set1_epi64(0x0E0C0A0806040200)); //don't care about odd 64-bit members, so can just broadcast
+			__m256i sh = _mm256_shuffle_epi8(a, _mm256_set1_epi64(0x0E'0C'0A'08'06'04'02'00)); //don't care about odd 64-bit members, so can just broadcast
 			//__m256i trunc1 = _mm256_and_si256(a, _mm256_set1_epi16(0xFF)); //force upper bytes of each word to zero
 			//__m256i packus = _mm256_packus_epi16(trunc1, trunc1); //upper 64-bit halves of each 128-bit lane are duplicated result
 			return TV::from_bits_us(_mm256_permute4x64_epi64(sh, 2 << 2)); //0, 2, 0, 0, upper discarded
