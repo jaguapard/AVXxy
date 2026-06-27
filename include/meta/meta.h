55def8094e2296a45f03b1cd3be9e856eca86555
jaguapard <77740043+jaguapard@users.noreply.github.com>
2026-06-28 01:01:28 +0500

    Renamed NATIVE_VEC_N to REG_LANE_COUNT_FOR

--- include/meta/meta.h
+++ include/meta/meta.h
@@ -200,16 +200,14 @@ namespace AVXXY_NAMESPACE
 		template<size_t N1, size_t N2>
 		concept SameSizeClasses = ((is_xmm_size(N1) && is_xmm_size(N2)) || (is_ymm_size(N1) && is_ymm_size(N2)) || (is_zmm_size(N1) && is_zmm_size(N2)));
 
-		//Native width of vector of type S, in elements (i.e. lane count of natively sized vector)
-		//Please note that NATIVE_VEC_N only considers sizes of the vectors.
-		//A "native" vector doesn't mean that operations on it are supported for full-width.  
-		//128-bit vectors are native to SSE1+
-		//256-bit for AVX+
-		//512-bit for AVX512-F+
-		//I.e. AVX will consider 256-bit integer vectors as "native", even though it can't do integer operations on them and will fall back to SSE
-		//This is done to not have a dissonance between vectors of different types, especially masks
-		//On the other hand, data movement operations are still valid even for "unsupported" types
-		template<typename S> requires IsScalarType<S> inline constexpr size_t NATIVE_VEC_N = []() {
+		//Returns the number of elements of type S that the largest architectual registers of current feature set can hold.
+		//Note that this in no way related to whether or not the operations on these vectors will be native or not.
+		//It is purely a numerical size quantity, equal to largest native vector width divided by the sizeof(S).
+		//Largest native vector widths are:
+		//128 bits for SSE and above
+		//256 bits for AVX and above
+		//512 bits for AVX512-F and above
+		template<typename S> requires IsScalarType<S> inline constexpr size_t REG_LANE_COUNT_FOR = []() {
 			using namespace internals;
 			/*
 			* 
